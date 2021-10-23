8468b838657a509d47dda468aa435dd72fcd822a
josegho2013 <josegho2013@gmail.com>
2021-10-23 14:32:39 -0300

    update

--- client/src/Components/CreateGame.js
+++ client/src/Components/CreateGame.js
@@ -1,14 +1,67 @@
-import React from 'react';
-
-
-
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 function CreateGame() {
-    return (
-      <div>
-        <h1>aqui va el formulario</h1>
-      </div>
-    );
-  };
+
+
+
   
+  return (
+    <div>
+      <div>
+        <div >
+          <h2>Create your New Game</h2>
+          <form>
+            <div>
+              <div>
+                <input required />
+              </div>
+              <div>
+                <input required />
+
+                <input required />
+              </div>
+              <div>
+                <input required />
+                <input required />
+              </div>
+              <div className="general">
+                <input required />
+
+                <input required />
+              </div>
+
+              <div>
+                <select>
+                 
+                </select>
+                <div>
+               
+                </div>
+              </div>
+            </div>
+
+            <div className="form_2">
+              <div className="image">
+                <input required />
+              </div>
+              <button type="submit">Create</button>
+            </div>
+          </form>
+        </div>
+
+        <p>Your dog was created successfully!</p>
+        <Link to="/home">
+          <button>Go Home</button>
+        </Link>
+      </div>
+
+     
+
+     
+    </div>
+  );
+}
+
 export default CreateGame;
