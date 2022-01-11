02c90729234b8bdd43416eec97e7b991c7ea0a4e
jose hernandez <josegho2013@gmail.com>
2022-01-11 19:05:45 -0300

    form

--- client/src/Components/CreateGame.js
+++ client/src/Components/CreateGame.js
@@ -3,49 +3,106 @@ import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 function CreateGame() {
+
+
+
+  
+
+
+
   return (
     <div>
-      <div>
+      <h2>Create Videogames</h2>
+      <form>
         <div>
-          <h2>Create your New Game</h2>
-          <form>
-            <div>
-              <div>
-                <input required />
-              </div>
-              <div>
-                <input required />
-                <input required />
-              </div>
-              <div>
-                <input required />
-                <input required />
-              </div>
-              <div className="general">
-                <input required />
-                <input required />
-              </div>
-
-              <div>
-                <select></select>
-                <div></div>
-              </div>
-            </div>
-
-            <div className="form_2">
-              <div className="image">
-                <input required />
-              </div>
-              <button type="submit">Create</button>
-            </div>
-          </form>
+          <label>Title :</label>
+          <input
+            type="text"
+            name="Title "
+            placeholder=" Insert Title "
+            value={input.Title }
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
         </div>
-
-        <p>Your game was created successfully!</p>
-        <Link to="/home">
-          <button>Go Home</button>
-        </Link>
-      </div>
+        <div>
+          <label>description</label>
+          <input
+            type="text"
+            name="description"
+            placeholder=" Insert description"
+            value={input.description}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label>Genre</label>
+          <input
+            type="text"
+            name="genre"
+            placeholder=" Insert genre"
+            value={input.genre}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label>Release year</label>
+          <input
+            type="text"
+            name="release-year"
+            placeholder=" Insert Release-year"
+            value={input.release-year}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label>Rating</label>
+          <input
+            type="text"
+            name="rating<"
+            placeholder=" Insert Rating<"
+            value={input.rating}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label>platform</label>
+          <input
+            type="text"
+            name="platform"
+            placeholder=" Insert Platform"
+            value={input.platform}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label>image</label>
+          <input
+            type="text"
+            name="image"
+            placeholder="Insert Image"
+            value={input.image}
+            onChange={(e) => handleChange(e)}
+            pattern="[a-zA-Z ]{3,15}"
+            required
+          />
+        </div>
+        <div>
+          <label></label>
+          <input />
+        </div>
+      </form>
     </div>
   );
 }
