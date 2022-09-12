951ea577a86753358cf3806fc482f4f6d25f76e6
jose hernandez <josegho2013@gmail.com>
2022-09-12 13:46:53 -0300

    Added dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,11 @@ import { BrowserRouter } from "react-router-dom";
 import store from "./Redux/store";
 import App from "./App";
 import "./index.css";
-import axios from "axios";
+import dotenv from "dotenv";
+import axios from "axios"
+dotenv.config();
 
-axios.defaults.baseURL = "http://localhost:3003";
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3003";
 
 ReactDOM.render(
   <Provider store={store}>
